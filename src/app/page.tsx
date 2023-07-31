f754ba2112de1b25cb533ea6205c3dd350c4572a
iuddin <8118095+iuddin@users.noreply.github.com>
2023-07-31 13:43:03 -0700

    Implement initial styling for the UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,14 @@ import React  from 'react';
 import { Bar, Line } from 'react-chartjs-2';
 import Image from 'next/image';
 import useProcessedData from './panel-one/page';
-import { faker } from '@faker-js/faker';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 
 import {
   Chart as ChartJS,
@@ -31,7 +38,11 @@ ChartJS.register(
   Legend
 );
 
-
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 
 export const options = {
@@ -78,21 +89,101 @@ export default function Home() {
   
 
   return (
-    <main className="main">
-      <div>
-        <h1>NaviStats</h1>
-      </div>
+    <ThemeProvider theme={darkTheme}>
+      <Box
+        sx={{
+        bgcolor: 'background.paper',
+        pt: 8,
+        pb: 6,
 
-      <div className="">
-        <br></br>
-        <p>Visualize data for global and US road traffic injuries and fatalities</p>
-        <p>Promote funding and development of autonomous driving</p>
-      </div>
-
-      <Bar 
-        options={options} 
-        data={data} 
-      />
-    </main>
+      }}
+      >
+        <main className="main">
+          <Box>
+            <Box>
+              <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="text.primary"
+                gutterBottom
+              >
+                NaviStats
+              </Typography>
+              <Typography
+                variant="h6" 
+                align="center" 
+                color="text.secondary" 
+                paragraph
+              >
+                Visualize data for global and US road traffic injuries and fatalities
+              </Typography>
+            </Box>
+          </Box>
+          <Container>
+            <Grid container spacing={2}>
+              <Grid item xs={4}>
+                <Card
+                  sx={{
+                  p: 5,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  height: 250,
+                  }}
+                >
+                  <p>About 1.3 million people die from road traffic crashes every year</p>
+                  <br></br>
+                  <p>20-50 million people suffer non-fatal injuries, many with resulting disabilities</p>
+                </Card>
+              </Grid>
+              <Grid item xs={4}>
+                <Paper
+                  sx={{
+                  p: 5,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  height: 250,
+                  }}
+                >
+                  <p>Road traffic injuries are the leading cause of deaths among children and young adults</p>
+                  <br></br>
+                  <p>Over half of road traffic deaths occur among vulnerable road users (pedestrians, cyclists, motorcyclists, etc.)</p>
+                </Paper>
+              </Grid>
+              <Grid item xs={4}>
+                <Paper
+                  sx={{
+                  p: 5,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  height: 250,
+                  }}
+                >
+                  <p>Risk factors for road traffic crashes</p>
+                  <p>- Speeding</p>
+                  <p>- Driving under influence</p>
+                  <p>- Distracted driving</p>
+                  <p>- Nonuse of seat belts, child restraints, motorcycle helmets</p>
+                </Paper>
+              </Grid>
+              <Grid item xs={12}>
+                <Paper 
+                  sx={{
+                    p: 0,
+                    display: 'flex',
+                    flexDirection: 'column'
+                  }}
+                >              
+                  <Bar 
+                    options={options} 
+                    data={data} 
+                  />
+                </Paper>    
+              </Grid>
+            </Grid>
+          </Container>
+        </main>
+      </Box>
+    </ThemeProvider>
   )
 }
