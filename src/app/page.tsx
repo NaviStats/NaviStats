9130d5a5ebbaf41618c309ab77db8daefc6c70c1
iuddin <8118095+iuddin@users.noreply.github.com>
2023-08-01 00:58:54 -0700

    Add Filter To Bar Chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React  from 'react';
+import React, { useEffect, useState }  from 'react';
 import { Bar, Line } from 'react-chartjs-2';
 import Image from 'next/image';
 import useProcessedData from './panel-one/page';
@@ -12,6 +12,10 @@ import Button from '@mui/material/Button';
 import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import Select, { SelectChangeEvent } from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
 
 import {
   Chart as ChartJS,
@@ -69,24 +73,124 @@ export const options = {
   barPercentage: 0.2,
 };
 
+function SelectRegion({region, setRegion}) {
+  const handleChange = (event: SelectChangeEvent) => {
+    setRegion(event.target.value as string);
+  };
 
-export default function Home() {
-  const globalDeathsByYear = useProcessedData();
+  return (
+    <Box>
+      <FormControl fullWidth>
+        <InputLabel>Select Region</InputLabel>
+        <Select
+          label="Region"
+          value={region}
+          onChange={handleChange}
+        >
+          <MenuItem value={'global'}>Global</MenuItem>
+          <MenuItem value={'USA'}>USA</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
 
-  const data = {
+function createDataObjForRegion(region, totalDeathsByYear) {
+  return {
     labels: ['2016', '2017', '2018', '2019'],
     datasets: [
       {
-        label: 'Absolute road deaths by year',
-        data: [globalDeathsByYear[2016], globalDeathsByYear[2017], globalDeathsByYear[2018], globalDeathsByYear[2019]],
+        label: `Absolute road deaths by year, ${region}`,
+        data: [totalDeathsByYear[2016], totalDeathsByYear[2017], totalDeathsByYear[2018], totalDeathsByYear[2019]],
         borderColor: 'rgba(52, 152, 219)',
         backgroundColor: 'rgba(52, 152, 219, 0.7)',
         barThickness: 70, 
         barPercentage: 1.0,
       }, 
     ],
-  };
+  }
+}
+
+function LoadingComponent() {
+  return(
+    <Typography>
+      Loading data...
+    </Typography>
+  )
+}
+
+export default function Home() {
+  const [region, setRegion] = useState('global');
+  const totalDeathsByYear = useProcessedData(region);
+  const [isLoading, setIsLoading] = useState(true);
+  console.log('parent home component, totalDeathsByYear', totalDeathsByYear)
+
+  useEffect(() => {
+    if (totalDeathsByYear && Object.keys(totalDeathsByYear).length > 0) {
+      setIsLoading(false);
+    }
+  }, [totalDeathsByYear])
+
+  //Approach 3
+  // const dataByRegion = {
+  //   region: isLoading ? null : createDataObjForRegion(region, totalDeathsByYear),
+  // }
+  // const data = dataByRegion[region];
+  // const data = isLoading ? null : createDataObjForRegion(region, totalDeathsByYear);
+  // console.log('data obj', data);
+
+  //Approach 1
+  const data = {
+    labels: ['2016', '2017', '2018', '2019'],
+    datasets: [
+      {
+        label: `Absolute road deaths by year, ${region}`,
+        data: [totalDeathsByYear[2016], totalDeathsByYear[2017], totalDeathsByYear[2018], totalDeathsByYear[2019]],
+        borderColor: 'rgba(52, 152, 219)',
+        backgroundColor: 'rgba(52, 152, 219, 0.7)',
+        barThickness: 70, 
+        barPercentage: 1.0,
+      }, 
+    ],
+  }
+
+  //Approach 2
+  // const [data, setData] = useState(
+  //   {
+  //     labels: ['2016', '2017', '2018', '2019'],
+  //     datasets: [
+  //       {
+  //         label: `Absolute road deaths by year, ${region}`,
+  //         data: [totalDeathsByYear[2016], totalDeathsByYear[2017], totalDeathsByYear[2018], totalDeathsByYear[2019]],
+  //         borderColor: 'rgba(52, 152, 219)',
+  //         backgroundColor: 'rgba(52, 152, 219, 0.7)',
+  //         barThickness: 70, 
+  //         barPercentage: 1.0,
+  //       }, 
+  //     ],
+  //   }
+  // );
   
+  // useEffect(() => {
+  //   setData(
+  //     {
+  //       labels: ['2016', '2017', '2018', '2019'],
+  //       datasets: [
+  //         {
+  //           label: `Absolute road deaths by year, ${region}`,
+  //           data: [totalDeathsByYear[2016], totalDeathsByYear[2017], totalDeathsByYear[2018], totalDeathsByYear[2019]],
+  //           borderColor: 'rgba(52, 152, 219)',
+  //           backgroundColor: 'rgba(52, 152, 219, 0.7)',
+  //           barThickness: 70, 
+  //           barPercentage: 1.0,
+  //         }, 
+  //       ],
+  //     }
+  //   )
+  // }, [totalDeathsByYear, region])
+
+
+  console.log('data obj', data);
 
   return (
     <ThemeProvider theme={darkTheme}>
@@ -110,16 +214,19 @@ export default function Home() {
               >
                 NaviStats
               </Typography>
-              <Typography
-                variant="h6" 
-                align="center" 
-                color="text.secondary" 
-                paragraph
-              >
-                Visualize data for global and US road traffic injuries and fatalities
-              </Typography>
+              {isLoading ? <LoadingComponent /> : 
+                <Typography
+                  variant="h6" 
+                  align="center" 
+                  color="text.secondary" 
+                  paragraph
+                >
+                  Visualize data for global and US road traffic injuries and fatalities
+                </Typography>
+              }
             </Box>
           </Box>
+          {isLoading ? null : (
           <Container>
             <Grid container spacing={2}>
               <Grid item xs={4}>
@@ -167,21 +274,26 @@ export default function Home() {
                 </Paper>
               </Grid>
               <Grid item xs={12}>
-                <Paper 
+                <Paper  
                   sx={{
                     p: 0,
                     display: 'flex',
                     flexDirection: 'column'
                   }}
-                >              
-                  <Bar 
-                    options={options} 
-                    data={data} 
-                  />
-                </Paper>    
+                >
+                <Bar 
+                  data={data}
+                  options={options} 
+                />          
+                </Paper>
+                <br></br>
+                <Grid item xs={2}>
+                  <SelectRegion region={region} setRegion={setRegion}/>
+                </Grid>    
               </Grid>
             </Grid>
           </Container>
+          )}
         </main>
       </Box>
     </ThemeProvider>
